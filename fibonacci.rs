d2bef268c91e78421ac9708e54c54399dffc776a
Ashish S <ashishjsharda@gmail.com>
2022-09-18 08:21:55 -0500

    Create fibonacci.rs

---
+++ fibonacci.rs
@@ -0,0 +1,20 @@
+
+const FIB_ZERO: u64 = 0;
+const FIB_ONE: u64 = 1;
+
+fn fib(n: u64) -> u64 {
+    if n == 0 {
+        FIB_ZERO
+    } else if n == 1 {
+        FIB_ONE
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+}
+
+fn main() {
+
+    println!("{}", fib(7));
+
+
+}
